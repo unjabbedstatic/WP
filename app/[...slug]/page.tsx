f555cc99f837ef696739c69aec9d089f1083188d
unjabbedstatic <unjabbedapp@gmail.com>
2025-08-11 00:10:54 +1000

    Update page.tsx

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -4,16 +4,10 @@ import fetchRendered from "../../lib/fetchRendered";
 export const dynamic = "force-static";
 export const revalidate = 3600;
 
-type Props = { params: { slug: string[] } };
+type Props = { params: { slug?: string[] } };
 
 export default async function CatchAll({ params }: Props) {
-  const path = "/" + params.slug.join("/");
-
-  // If your fetchRendered returns { bodyHtml }:
+  const path = "/" + (params.slug?.join("/") ?? "");
   const { bodyHtml } = await fetchRendered(path);
   return <main dangerouslySetInnerHTML={{ __html: bodyHtml }} />;
-
-  // If fetchRendered returns a plain string instead, use this:
-  // const html = await fetchRendered(path);
-  // return <main dangerouslySetInnerHTML={{ __html: html }} />;
 }
