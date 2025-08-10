50c395eb2efece115f8cfe0efe70b143f32eaabd
unjabbedstatic <unjabbedapp@gmail.com>
2025-08-11 00:50:00 +1000

    Update page.tsx

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -6,9 +6,11 @@ export const revalidate = 3600;
 
 type Props = { params: { slug?: string[] } };
 
-export default async function CatchAll({ params }: Props) {
-  const path =
-    params.slug && params.slug.length ? `/${params.slug.join("/")}` : "/";
-  const { bodyHtml } = await fetchRendered(path);
+export default async function Page({ params }: Props) {
+  const path = params.slug?.length ? `/${params.slug.join("/")}` : "/";
+
+  const { bodyHtml /*, bodyClass*/ } = await fetchRendered(path);
+
+  // You can add className={bodyClass} to <main> if you want WP body classes.
   return <main dangerouslySetInnerHTML={{ __html: bodyHtml }} />;
 }
