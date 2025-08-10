d2de026a92ef0158bf9ea3b414044bdba69f74cf
unjabbedstatic <unjabbedapp@gmail.com>
2025-08-10 22:42:51 +1000

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/page.tsx
-import fetchRendered from "../lib/fetchRendered";
-
-export const dynamic = "force-dynamic"; // render at request time
-
-export default async function Page() {
-  const { body } = await fetchRendered("/");
-  return <main dangerouslySetInnerHTML={{ __html: body }} />;
-}
