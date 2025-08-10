c4628032f021fe47c61704a75c30d70c030b17af
unjabbedstatic <unjabbedapp@gmail.com>
2025-08-10 23:48:31 +1000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,45 @@
 // app/layout.tsx
-import type { ReactNode } from "react";
+import Script from "next/script";
 import fetchRendered from "../lib/fetchRendered";
 import ScriptLoader from "./ScriptLoader";
 
 export const dynamic = "force-static";
-export const revalidate = 3600;
 
-export default async function RootLayout({ children }: { children: ReactNode }) {
-  const { head, bodyClass, styles, scripts } = await fetchRendered("/");
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const r = await fetchRendered("/");
 
   return (
     <html lang="en">
-      <head dangerouslySetInnerHTML={{ __html: head }} />
-      <body className={bodyClass} suppressHydrationWarning>
-        {/* Ensure CSS is definitely loaded */}
-        {styles.map((href) => (
-          <link key={href} rel="stylesheet" href={href} />
+      <head suppressHydrationWarning>
+        {/* WordPress styles from <head> */}
+        {r.headLinks.map((attrs, i) => (
+          // eslint-disable-next-line @next/next/no-css-tags
+          <link key={`hl-${i}`} {...attrs} />
+        ))}
+        {r.headStyleTags.map((css, i) => (
+          <style key={`hs-${i}`} dangerouslySetInnerHTML={{ __html: css }} />
         ))}
 
-        {/* Make WP scripts run (needed for FAQ accordions, etc.) */}
-        <ScriptLoader srcs={scripts} />
+        {/* Order-sensitive head scripts (e.g., jQuery before plugins) */}
+        {r.headScriptSrcs.map((src, i) => (
+          <Script key={`hss-${i}`} src={src} strategy="beforeInteractive" />
+        ))}
+        {r.headInlineScripts.map((code, i) => (
+          <Script
+            key={`his-${i}`}
+            id={`head-inline-${i}`}
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{ __html: code }}
+          />
+        ))}
+      </head>
+
+      <body className={r.bodyClass} suppressHydrationWarning>
+        {/* Load body scripts in order, then inline ones */}
+        <ScriptLoader srcs={r.bodyScriptSrcs} />
+        {r.bodyInlineScripts.map((code, i) => (
+          <Script key={`bis-${i}`} id={`body-inline-${i}`} strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: code }} />
+        ))}
 
         {children}
       </body>
