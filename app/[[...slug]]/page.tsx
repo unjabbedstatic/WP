70a4f64c231cb94a6d6b1af36e64b4d5791703cc
unjabbedstatic <unjabbedapp@gmail.com>
2025-08-10 23:51:44 +1000

    Update page.tsx

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -4,15 +4,10 @@ import fetchRendered from "../../lib/fetchRendered";
 export const dynamic = "force-static";
 export const revalidate = 3600;
 
-type Props = {
-  params: { slug?: string[] };
-};
+type Props = { params: { slug?: string[] } };
 
-export default async function CatchAllPage({ params }: Props) {
-  const path =
-    params.slug && params.slug.length > 0 ? `/${params.slug.join("/")}` : "/";
-
-  const { html } = await fetchRendered(path);
-
-  return <main dangerouslySetInnerHTML={{ __html: html }} />;
+export default async function CatchAll({ params }: Props) {
+  const path = params?.slug?.length ? `/${params.slug.join("/")}` : "/";
+  const { bodyHtml } = await fetchRendered(path);
+  return <main dangerouslySetInnerHTML={{ __html: bodyHtml }} />;
 }
