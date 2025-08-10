18d3bde2e3d50bbdf10843f4dea9929ff9bef782
unjabbedstatic <unjabbedapp@gmail.com>
2025-08-10 20:45:14 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import fetchRendered from "../lib/fetchRendered";
 export const dynamic = "force-static";
 
 export default async function Page() {
-  const html = await fetchRendered("/");
-  return <main dangerouslySetInnerHTML={{ __html: html }} />;
+  const { body } = await fetchRendered("/");
+  // body MUST be a string and we MUST pass { __html: body }
+  return <main dangerouslySetInnerHTML={{ __html: body }} />;
 }
